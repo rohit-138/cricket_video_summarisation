769f03d52c34fa5ea7306b690650a3a21ef0b4dc
Rohit <khalaterohit123@gmail.com>
2024-02-05 09:46:04 +0530

    deleted some files

--- CODE/temp.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-model = YOLO('../Model/bowler.pt')
-
-video_path = "D:\BE Final Year Project\inputs/nine.mp4"
-
-model.predict(source=video_path,save=True,conf=0.4,project='./Storage/output')
